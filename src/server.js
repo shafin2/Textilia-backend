05f2e9232f3d7e58ead2d5d3a8b639fdf81b2664
Shafin zaman <shafinzaman42@gmail.com>
2024-10-03 15:12:57 +0500

    Changes for deployement

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./env/local.env" });
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
@@ -6,19 +6,22 @@ const connectDB = require("./config/db");
 const app = express();
 
 var corsOptions = {
-	origin: process.env.REMOTE_CLIENT_URL,
-	optionsSuccessStatus: 200,
-	credentials: true,
+  origin: process.env.REMOTE_CLIENT_URL,
+  optionsSuccessStatus: 200,
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Connect to the database
 connectDB();
 
+// Routes
 const routes = require("./routes");
 app.use("/api", routes);
 
+// Use the port from environment variables or default to 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
